fada36bcdb7afab6517c196754d45c34603dc92b
Cristian Golea <Cristian.golea10@gmail.com>
2024-01-18 14:26:19 +0200

    initial dashboard created

---
+++ MoneyTrackerApp/Controllers/DashboardController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MoneyTrackerApp.Models;
+
+namespace MoneyTrackerApp.Controllers
+{
+    public class DashboardController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public DashboardController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<ActionResult> Index()
+        {
+
+            //last 7 days
+            DateTime StartDate= DateTime.Today.AddDays(-6);
+            DateTime EndDate = DateTime.Today;
+
+            List<Transaction> SelectedTransactions = await _context.Transactions
+                .Include(x => x.Category)
+                .Where(y => y.Date >= StartDate && y.Date <= EndDate)
+                .ToListAsync();
+
+            //Total Income
+            int TotalIncome = SelectedTransactions
+                .Where(i => i.Category.Type == "Income")
+                .Sum(j => j.Amount);
+            ViewBag.TotalIncome = TotalIncome.ToString("C0");
+
+            //Total Expense
+            int TotalExpense = SelectedTransactions
+                .Where(i => i.Category.Type == "Expense")
+                .Sum(j => j.Amount);
+            ViewBag.TotalExpense = TotalExpense.ToString("C0");
+
+            //Total Balance
+            int TotalBalance = TotalIncome - TotalExpense;
+            ViewBag.TotalBalance = TotalBalance.ToString("C0");
+
+            return View();
+        }
+    }
+}
